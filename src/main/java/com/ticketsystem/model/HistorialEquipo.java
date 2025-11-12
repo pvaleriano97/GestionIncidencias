9cee6c9dbb538c16f907689b31cb54fe2b626996
mariasv2025 <u20239206@utp.edu.pe>
2025-11-11 19:47:27 -0500

    model informe tecnico

--- src/main/java/com/ticketsystem/model/HistorialEquipo.java
+++ src/main/java/com/ticketsystem/model/HistorialEquipo.java
@@ -4,10 +4,31 @@
  */
 package com.ticketsystem.model;
 
+import java.sql.Timestamp;
+
 /**
  *
  * @author ROSA SANDOVAL
  */
 public class HistorialEquipo {
+    private int idHistorial;
+    private int idEquipo;
+    private String detalle;
+    private Timestamp fechaRegistro; // Campo asumido para tracking
     
+    // Constructor, Getters y Setters
+    public HistorialEquipo() {}
+
+    public int getIdHistorial() { return idHistorial; }
+    public void setIdHistorial(int idHistorial) { this.idHistorial = idHistorial; }
+
+    public int getIdEquipo() { return idEquipo; }
+    public void setIdEquipo(int idEquipo) { this.idEquipo = idEquipo; }
+
+    public String getDetalle() { return detalle; }
+    public void setDetalle(String detalle) { this.detalle = detalle; }
+
+    public Timestamp getFechaRegistro() { return fechaRegistro; }
+    public void setFechaRegistro(Timestamp fechaRegistro) { this.fechaRegistro = fechaRegistro; }
 }
+
