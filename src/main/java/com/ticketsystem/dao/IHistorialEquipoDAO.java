2406b72d6f2977d72541cf45883c9e73c1092a65
mariasv2025 <u20239206@utp.edu.pe>
2025-11-13 20:05:49 -0500

    Update IHistorialEquipoDAO.java

--- src/main/java/com/ticketsystem/dao/IHistorialEquipoDAO.java
+++ src/main/java/com/ticketsystem/dao/IHistorialEquipoDAO.java
@@ -3,12 +3,19 @@ package com.ticketsystem.dao;
 
 import com.ticketsystem.model.HistorialEquipo;
 import java.util.List;
+
 public interface IHistorialEquipoDAO {
-    List<HistorialEquipo> listar(int page, int size, String filtro) throws Exception;
-    int contar(String filtro) throws Exception;
-    HistorialEquipo buscarPorId(int id) throws Exception;
-    boolean insertar(HistorialEquipo h) throws Exception;
+    
+boolean insertar(HistorialEquipo h) throws Exception;
+
     boolean actualizar(HistorialEquipo h) throws Exception;
+
     boolean eliminar(int id) throws Exception;
-    List<HistorialEquipo> listar(); // para combos sin paginación
+
+    HistorialEquipo buscarPorId(int id) throws Exception;
+
+    List<HistorialEquipo> listar() throws Exception;
+
+    List<HistorialEquipo> listarPorEquipo(int idEquipo) throws Exception;
 }
+
